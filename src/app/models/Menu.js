6c811a933559b167900d7605e451e89ba4c96dbc
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2021-11-24 21:26:00 +0700

    Update Menu.js

--- src/app/models/Menu.js
+++ src/app/models/Menu.js
@@ -3,21 +3,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Menu = new Schema({
-    id:{ type: Number},//id sản phẩm
-    category:{ type: String, maxlength:50 },//nhóm sản phẩm
-    name:{ type: String, maxlength:100  },//tên
-    image:{ type: String, maxlength:600 },//link hình ảnh
-    old_price:{ type: Number},//giá ban đầu
-    new_price:{ type: Number},//giá mới 
-    rating:{ type: Number },//điểm đánh giá
+    id:{ type: Number},
+    category:{ type: String, maxlength:50 },
+    name:{ type: String, maxlength:100  },
+    image:{ type: String, maxlength:600 },
+    old_price:{ type: Number},
+    new_price:{ type: Number},
+    rating:{ type: Number },
+    in_stock:{ type: Number },
     num_rating:{ type: Number },
-    in_stock:{ type: Number },//
     slug:{ type: String, maxlength:100 },
     description:{ type: String, maxlength:600  }
     
 });
 
 
-    // creatAt: { type:Date, default:Date.now },
+// creatAt: { type:Date, default:Date.now },
     // updateAt: { type:Date, default:Date.now }
 module.exports = mongoose.model('menus', Menu);
